cc24f82d1ade1a1b352f40550870b66291e04768
avi <avivbanerjee2000@gmail.com>
2025-04-26 02:15:05 +0530

    testing is set up

---
+++ normalize_url.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"net/url"
+)
+
+func normalizeURL(original_url string) (string, error) {
+	parsedUrl, err := url.Parse(original_url)
+	if err != nil {
+		return "", err
+	}
+	normalizedUrl := parsedUrl.Hostname() + parsedUrl.EscapedPath()
+	fmt.Println(normalizedUrl)
+	return normalizedUrl, nil
+}
